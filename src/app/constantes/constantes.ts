e577f8658d5159c537b00a7173f271a087b8df0b
MohamedBachir <bachir.hadjadji@naftal.dz>
2023-03-02 10:59:03 +0100

    edit const

--- src/app/constantes/constantes.ts
+++ src/app/constantes/constantes.ts
@@ -1,5 +1,5 @@
 export const colors = ["50A060","7b68ee","5f48ea","E248AA","3498DB","F57C01"] 
-export const baseUrl = "http://192.168.0.127:8000/api/"
+export const baseUrl = "http://10.80.100.252:8000/api/"
 export function randomColor<String>(){
     const  color = colors[Math.floor(Math.random() * colors.length)];
     return color;
